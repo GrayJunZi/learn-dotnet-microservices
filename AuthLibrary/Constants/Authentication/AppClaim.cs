a7097d440a715d8ac1534b9131e11e14ac0c0847
GrayJunZi <GrayJunZi@outlook.com>
2025-07-01 07:38:47 +0800

    AuthLibrary

---
+++ AuthLibrary/Constants/Authentication/AppClaim.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthLibrary.Constants.Authentication
+{
+    public static class AppClaim
+    {
+        public const string Permission = "permission";
+        public const string Expiration = "exp";
+        public const string Issuer = "gateway";
+    }
+}
---
+++ AuthLibrary/Permissions/PermissionPolicyProvider.cs
@@ -0,0 +1,39 @@
+﻿using AuthLibrary.Constants.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+
+namespace AuthLibrary.Permissions
+{
+    public class PermissionPolicyProvider : IAuthorizationPolicyProvider
+    {
+        public DefaultAuthorizationPolicyProvider FallbackPolicyProvider { get; }
+
+        public PermissionPolicyProvider(IOptions<AuthorizationOptions> options)
+        {
+            FallbackPolicyProvider = new DefaultAuthorizationPolicyProvider(options);
+        }
+
+        public Task<AuthorizationPolicy> GetDefaultPolicyAsync()
+        {
+            return FallbackPolicyProvider.GetDefaultPolicyAsync();
+        }
+
+        public Task<AuthorizationPolicy?> GetFallbackPolicyAsync()
+        {
+            return Task.FromResult<AuthorizationPolicy?>(null);
+        }
+
+        public Task<AuthorizationPolicy?> GetPolicyAsync(string policyName)
+        {
+            if (policyName.StartsWith(AppClaim.Permission, StringComparison.CurrentCultureIgnoreCase))
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .AddRequirements(new PermissionRequirement(policyName))
+                    .Build();
+                return Task.FromResult(policy);
+            }
+
+            return FallbackPolicyProvider.GetPolicyAsync(policyName);
+        }
+    }
+}
